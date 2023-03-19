05341a8380b17b8a08d449703425adac42978ab9
Bandana <gbandana21@tbc.edu.np>
2023-03-19 14:23:21 +0545

    Dice roll Practise

--- app/src/main/java/com/example/dmalab1/MainActivity.java
+++ app/src/main/java/com/example/dmalab1/MainActivity.java
@@ -6,34 +6,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.dmalab1.Model.Dice;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView diceNumber;
-    private Button rollButton;
-
+    private Button rollBtn;
+    private TextView textView;
+    private TextView textViewSecond;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rollBtn = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textViewSecond = findViewById(R.id.textView_second);
 
-        diceNumber = findViewById(R.id.diceNumber);
-        rollButton = findViewById(R.id.rollButton);
-
-        rollButton.setOnClickListener(new View.OnClickListener() {
+        rollBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // Generate a random number between 1 and 6
-                Random rand = new Random();
-                int randomNumber = rand.nextInt(6) + 1;
-
-                // Update the text view with the random number
-                diceNumber.setText(Integer.toString(randomNumber));
-
-                // Show a toast message
-                Toast.makeText(MainActivity.this, "Dice rolled", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                rollDice();
             }
+
         });
+    }
+
+    public void rollDice()
+    {
+        Toast toast = Toast.makeText(MainActivity.this, "Dice Rolled!", Toast.LENGTH_SHORT );
+        Dice dice = new Dice(6);
+        textView.setText(Integer.toString(dice.roll()));
+        textViewSecond.setText(Integer.toString(dice.roll()));
+        toast.show();
     }
 }
